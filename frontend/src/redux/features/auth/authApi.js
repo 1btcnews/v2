eefe411d4b1c7f46314450bfa2405a2dd13eaf23
1btcnews <bvgf977@gmail.com>
2024-12-29 11:01:41 +0600

    Update authApi.js

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3000/api/user',
+        baseUrl: 'https://v2-1v9p970px-1btcnews-projects.vercel.app/api/user',
         credentials: 'include',
     }),
     endpoints: (builder) => ({
