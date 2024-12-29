20d762d6851165dedbd6e13f1af7cc77d91d62e0
1btcnews <bvgf977@gmail.com>
2024-12-29 20:05:30 +0600

    Delete backend/src directory

--- backend/src/middleware/isAdmin.js
+++
@@ -1,9 +0,0 @@
-const isAdmin = (req, res, next) => {
-    if (req.role !== 'admin') {
-        return res.status(403).json({ success: false, message: 'You are not authorized to perform this action.' });
-    }
-    next();
-};
-
-
-export default isAdmin;
