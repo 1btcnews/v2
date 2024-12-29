6e0524e1db0892d95a4fb4fa9d1be926b79cdc75
1btcnews <bvgf977@gmail.com>
2024-12-29 22:47:17 +0600

    Update authApi.js

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'localhost:3000/api/user',
+        baseUrl: 'https://v2-ten-kappa.vercel.app/api/user',
         credentials: 'include',
     }),
     endpoints: (builder) => ({
