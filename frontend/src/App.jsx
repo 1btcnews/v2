3e2ae8fe4390f758e54a7871fd6eef5dd2078bbd
1btcnews <bvgf977@gmail.com>
2024-12-30 09:44:30 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {Outlet} from'react-router-dom'
 import Navbar from './components/Navbar'
-import { Analytics } from '@vercel/analytics/next';
+//import { Analytics } from '@vercel/analytics/next';
 import Footer from './components/Footer'
 import './App.css'
 
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-     <Analytics />
+{/*      <Analytics /> */}
      <div className=' min-h-screen flex flex-col bg-slate-300'>
        <Navbar/>
        <div className='flex-grow lol'> <Outlet/> </div>
