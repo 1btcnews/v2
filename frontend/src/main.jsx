6a92229b93ae63a59e26b2b86752de977b301cf1
1btcnews <bvgf977@gmail.com>
2024-12-30 09:38:47 +0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Analytics } from '@vercel/analytics/next';
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import {store} from './redux/store.js'
@@ -9,5 +10,6 @@ import router from './router/router.jsx'
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <RouterProvider router={router}/>
+    <Analytics />
   </Provider>,
 )
