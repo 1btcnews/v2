ec20db02783918b7bcb4d685659aa0bd2b388c19
1btcnews <bvgf977@gmail.com>
2024-12-30 09:42:20 +0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Analytics } from '@vercel/analytics/next';
+//import { Analytics } from '@vercel/analytics/next';
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import {store} from './redux/store.js'
@@ -10,6 +10,6 @@ import router from './router/router.jsx'
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <RouterProvider router={router}/>
-    <Analytics />
+{/*    // <Analytics /> */}
   </Provider>,
 )
